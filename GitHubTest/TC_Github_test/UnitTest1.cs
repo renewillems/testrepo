27ac52e3fc0250aa1e9d8627713764031d1c8da7
renewillems <33660714+renewillems@users.noreply.github.com>
2017-11-27 16:15:22 +0100

    8=8

--- GitHubTest/TC_Github_test/UnitTest1.cs
+++ GitHubTest/TC_Github_test/UnitTest1.cs
@@ -9,7 +9,13 @@ namespace TC_Github_test
     [TestMethod]
     public void TestMethod1()
     {
-      Assert.AreEqual(6,6);
+      Assert.AreEqual(6, 6);
+    }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+      Assert.AreEqual(8, 8);
     }
   }
 }
