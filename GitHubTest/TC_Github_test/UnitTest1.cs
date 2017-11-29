60af18b6c876ceadf6c99b2845d218954249c243
renewillems <33660714+renewillems@users.noreply.github.com>
2017-11-29 12:07:53 +0100

    100 = 100

--- GitHubTest/TC_Github_test/UnitTest1.cs
+++ GitHubTest/TC_Github_test/UnitTest1.cs
@@ -7,30 +7,6 @@ namespace TC_Github_test
   public class UnitTest1
   {
     [TestMethod]
-    public void TestMethod1()
-    {
-      Assert.AreEqual(6, 6);
-    }
-
-    [TestMethod]
-    public void TestMethod2()
-    {
-      Assert.AreEqual(8, 8);
-    }
-
-    [TestMethod]
-    public void TestMethod3()
-    {
-      Assert.AreEqual(9, 9);
-    }
-
-    [TestMethod]
-    public void TestMethod4()
-    {
-      Assert.AreEqual(28, 28);
-    }
-
-    [TestMethod]
     public void TestMethod5()
     {
       Assert.AreEqual(100, 100);
