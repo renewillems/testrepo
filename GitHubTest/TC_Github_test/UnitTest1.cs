73f304c73cf9f6816449aaa1bcf4ee6345de56c7
renewillems <33660714+renewillems@users.noreply.github.com>
2017-11-29 16:17:55 +0100

    Added new testcases

--- GitHubTest/TC_Github_test/UnitTest1.cs
+++ GitHubTest/TC_Github_test/UnitTest1.cs
@@ -6,10 +6,32 @@ namespace TC_Github_test
   [TestClass]
   public class UnitTest1
   {
+    [TestCategory("Testset_001")]
     [TestMethod]
-    public void TestMethod5()
+    public void TestMethod1()
     {
-      Assert.AreEqual(301, 301);
+      Assert.AreEqual(1, 1);
+    }
+
+    [TestCategory("Testset_002")]
+    [TestMethod]
+    public void TestMethod2()
+    {
+      Assert.AreEqual(2, 2);
+    }
+
+    [TestCategory("Testset_003")]
+    [TestMethod]
+    public void TestMethod3()
+    {
+      Assert.AreEqual(3, 3);
+    }
+
+    [TestCategory("Testset_004")]
+    [TestMethod]
+    public void TestMethod4()
+    {
+      Assert.AreEqual(4, 4);
     }
   }
 }
