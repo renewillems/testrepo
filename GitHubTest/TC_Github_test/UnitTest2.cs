0b5ba0d9703a752b3b6465dbf69577ad8b5ba1c4
renewillems <33660714+renewillems@users.noreply.github.com>
2017-12-11 14:27:09 +0100

    Added Xunit test

---
+++ GitHubTest/TC_Github_test/UnitTest2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace TC_Github_test
+{
+  public class MyXUnitTest
+  {
+    [Fact]
+    public static void XUnitTest()
+    {
+      Assert.Equal(5, 5);
+    }
+  }
+}
